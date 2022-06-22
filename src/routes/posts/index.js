14a51eceace89b30260880bec5fdeebe67f402df
sibras-accelr <mohamed.sibras@accelr.net>
2022-06-22 21:37:55 +0530

    Initial commit

---
+++ src/routes/posts/index.js
@@ -0,0 +1,22 @@
+// @ts-ignore
+import { client } from '$lib/graphql/graphql-client'
+// @ts-ignore
+import { getAllPosts } from '$lib/graphql/graphql-queries'
+
+/** @type {import('@sveltejs/kit').RequestHandler} */
+export const get = async () => {
+  try {
+    const { posts } = await client.request(getAllPosts)
+
+    return {
+      status: 200,
+      body: { posts },
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      // @ts-ignore
+      body: { error: error.message },
+    }
+  }
+}
